53ecb494b26099c53c07f4ca2c1484b22caf388b
med-am1n <chelali.bavarische@gmail.com>
2026-05-11 20:20:27 +0100

    implement basic shell input loop

--- main.c
+++ main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
-
+#include <string.h>
 int main() {
-    printf("Hello, World!");
+    char input[1024];
+
+
+    while(1){
+        printf( "mini-shell> " );
+
+        if (fgets(input, sizeof(input), stdin) == NULL) {
+            break;
+        }
+
+        // Remove trailing newline
+        input[strcspn(input, "\n")] = '\0';
+
+        printf("input: %s\n", input);
+    }
+
     return 0;
 }
