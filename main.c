213db0886fdcf409ac478dfa615b5302e686f437
med-am1n <chelali.bavarische@gmail.com>
2026-05-11 21:53:06 +0100

    fix implement the main loop

--- main.c
+++ main.c
@@ -40,62 +40,63 @@ void free_args(char **args)
 int main()
 {
     char input[1024];
-
-    printf("mini-shell> ");
-
-    if (fgets(input, sizeof(input), stdin) == NULL)
+    while (1)
     {
-        return 1;
-    }
+        printf("mini-shell> ");
 
-    input[strcspn(input, "\n")] = '\0';
-
-    char **args = tokenize(input);
-
-    // for (int i = 0; args[i] != NULL; i++) {
-    //     printf("arg[%d] = %s\n", i, args[i]);
-    // }
-
-    if (strcmp(args[0], "exit") == 0)
-    {
-        free_args(args);
-        return 0;
-    }
-
-    else if (strcmp(args[0], "cd") == 0)
-    {
-        const char *path = args[1] ? args[1] : getenv("HOME");
-        if (path == NULL)
+        if (fgets(input, sizeof(input), stdin) == NULL)
         {
-            fprintf(stderr, "cd: expected argument\n");
+            return 1;
         }
-        else
+
+        input[strcspn(input, "\n")] = '\0';
+        
+        char **args = tokenize(input);
+
+        // for (int i = 0; args[i] != NULL; i++) {
+        //     printf("arg[%d] = %s\n", i, args[i]);
+        // }
+
+        if (strcmp(args[0], "exit") == 0)
         {
-            if (chdir(path) != 0)
+            free_args(args);
+            return 0;
+        }
+
+        else if (strcmp(args[0], "cd") == 0)
+        {
+            const char *path = args[1] ? args[1] : getenv("HOME");
+            if (path == NULL)
             {
-                perror("cd");
+                fprintf(stderr, "cd: expected argument\n");
+            }
+            else
+            {
+                if (chdir(path) != 0)
+                {
+                    perror("cd");
+                }
             }
         }
-    }
 
-    else
-    {
-        pid_t pid = fork();
-
-        if (pid == 0)
-        {
-            execvp(args[0], args);
-            perror("execvp");
-            exit(1);
-        }
         else
         {
-            wait(NULL);
-        }
-    }
+            pid_t pid = fork();
 
-    // Free memory
-    free_args(args);
+            if (pid == 0)
+            {
+                execvp(args[0], args);
+                perror("execvp");
+                exit(1);
+            }
+            else
+            {
+                wait(NULL);
+            }
+        }
+        // Free memory
+        free_args(args);
+    }
 
     return 0;
 }
